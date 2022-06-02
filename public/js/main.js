567871d1177f6879ab9ac74e5fd612625bb29dae
Alejandro Ibáñez Hurtado <aibahur653@g.educaand.es>
2022-06-03 01:42:58 +0200

    v2.2

--- public/js/main.js
+++ public/js/main.js
@@ -226,5 +226,10 @@ jQuery(document).ready(function($) {
 				
 	// };
 	// siteCountDown();
+	$('.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+      }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+      });
 
 });
